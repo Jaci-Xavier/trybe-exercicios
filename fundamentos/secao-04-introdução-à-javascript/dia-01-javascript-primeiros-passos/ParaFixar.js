71ecbc9a723ac5fa3e2a2d847e1dda18f2a87928
Jaci Xavier <ourivesjr@gmail.com>
2022-12-19 20:58:02 -0300

    paraFixar03Dia01

--- fundamentos/secao-04-introdução-à-javascript/dia-01-javascript-primeiros-passos/ParaFixar.js
+++ fundamentos/secao-04-introdução-à-javascript/dia-01-javascript-primeiros-passos/ParaFixar.js
@@ -1,10 +1,22 @@
+//para fixar 1
 const myName = 'Jaci';
 const birthCity = 'Rio Bonito';
 let birthYear = 2030;
 
-// birthCity = 'rio de janeiro';
+birthCity = 'rio de janeiro';
 birthYear = 1990;
 
 console.log(myName);
 console.log(birthCity);
 console.log(birthYear);
+
+//para fixar 2
+
+const base = 5;
+const heigth = 8;
+const area = base * heigth;
+console.log(area);
+const perimeter = (5 * 2) + (8 * 2);
+console.log(perimeter);
+
+//para fixar 3
