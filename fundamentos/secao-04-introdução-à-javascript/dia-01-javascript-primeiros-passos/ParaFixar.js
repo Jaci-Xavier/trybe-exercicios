25445b1793e1cc4f9bf118b302a15aba8cd1380d
Jaci Xavier <ourivesjr@gmail.com>
2022-12-19 22:09:39 -0300

    paraFixar05

--- fundamentos/secao-04-introdução-à-javascript/dia-01-javascript-primeiros-passos/ParaFixar.js
+++ fundamentos/secao-04-introdução-à-javascript/dia-01-javascript-primeiros-passos/ParaFixar.js
@@ -48,3 +48,13 @@ if (currentHour >= 22) {
 };
 
 console.log(message);
+
+// para fixar 5
+
+let weekDay = 'quarta-feira'
+if (weekDay === 'segunda-feira' || weekDay === 'terça-feira' || weekDay === 'quarta-feira' || weekDay === 'quinta-feira' || weekDay === 'sexta-feira') {
+  console.log('Oba, mais um dia de aprendizado na Trybe >;D')
+} else {
+  console.log('FINALMENTE, descanso merecido UwU')
+};
+
