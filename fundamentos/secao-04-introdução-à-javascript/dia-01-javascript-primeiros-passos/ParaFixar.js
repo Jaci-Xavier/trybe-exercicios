b872b0643c3d409a8ec579b4a90944fe0c6e397f
Jaci Xavier <ourivesjr@gmail.com>
2022-12-19 21:37:55 -0300

    paraFixar04

--- fundamentos/secao-04-introdução-à-javascript/dia-01-javascript-primeiros-passos/ParaFixar.js
+++ fundamentos/secao-04-introdução-à-javascript/dia-01-javascript-primeiros-passos/ParaFixar.js
@@ -30,3 +30,21 @@ if (media >= 80) {
 } else if (media < 60) {
   console.log("Infelizmente, você reprovou.");
 }
+
+// para fixar 4
+
+const currentHour = 20;
+let message = '';
+if (currentHour >= 22) {
+  mesage = 'Não deveríamos comer nada, é hora de dormir'
+} else if (currentHour >= 18 && currentHour < 22) {
+  message = 'Rango da noite, vamos jantar :D';
+} else if ( currentHour >= 14 && currentHour < 18) {
+  message = 'Vamos fazer um bolo pro café da tarde?'
+} else if ( currentHour >= 11 && currentHour < 14) {
+  message = 'Hora do almoço !!!'
+} else if ( currentHour >= 4 && currentHour < 11) {
+  message = 'Hmmm, cheiro de café recém-passado'
+};
+
+console.log(message);
