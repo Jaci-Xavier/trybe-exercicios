285e94d10f7a12ea11ae6d5bc13a449e71323089
Jaci Xavier <ourivesjr@gmail.com>
2022-12-19 23:45:01 -0300

    resolvendo exercicios

---
+++ fundamentos/secao-04-introdução-à-javascript/dia-01-javascript-primeiros-passos/exercicios.js
@@ -0,0 +1,137 @@
+const a = 8;
+const b = 3;
+const c = 2;
+
+console.log("Adição: " + (a + b));
+console.log("Subtração: " + (a - b));
+console.log("Multiplicação: " + a * b);
+console.log("Divisão: " + a / b);
+console.log("Modulo: " + (a % b));
+if (a > b) {
+  console.log('"a" é maior que "b"');
+} else {
+  console.log('"a" é menor que "b"');
+}
+
+if (a > b && a > c) {
+  console.log("O maior número é: " + a + " (a)");
+} else if (b > a && b > c) {
+  console.log("O maior número é: " + b + " (b)");
+} else {
+  console.log("O maior número é: " + c + " (c)");
+};
+
+if (c > 0) {
+    console.log('positive');
+  } else if (number < 0) {
+    console.log('negative');
+  } else {
+    console.log('zero');
+};
+
+//
+
+const degreeAngleA = 50;
+const degreeAngleB = 100;
+const degreeAngleC = 30;
+
+let sumOfAngles = degreeAngleA + degreeAngleB + degreeAngleC;
+
+let allAnglesArePositives = degreeAngleA > 0 && degreeAngleB > 0 && degreeAngleC > 0;
+
+if(allAnglesArePositives){
+  if (sumOfAngles === 180) {
+    console.log(true);
+  } else {
+    console.log(false);
+  };
+} else {
+  console.log('Erro: ângulo inválido');
+};
+
+//
+
+const chessPiece = 'bispo';
+
+switch (chessPiece.toLowerCase()) {
+  case 'rei':
+    console.log('Rei-> Uma casa apenas para qualquer direção.');
+    break;
+  case 'bispo':
+    console.log('Bispo-> Diagonal.');
+    break;
+  case 'rainha':
+    console.log('Rainha-> Diagonal, horizontal e vertical.');
+    break;
+  case 'cavalo':
+    console.log('Cavalo -> "L" pode pular sobre as peças.');
+    break;
+  case 'torre':
+    console.log('Torre -> Horizontal e vertical.');
+    break;
+  case 'peão':
+    console.log("Peão -> Apenas uma casa para frente, no primeiro movimento podem ser duas casas.");
+    break;
+  default:
+    console.log('Erro, peça inválida!');
+    break;
+};
+
+//
+
+const média = 90;
+
+if (média < 0 || média > 100) {
+  console.log("Erro, nota incorreta!");
+} else if (média >= 90) {
+  console.log("A");
+} else if (média >= 80) {
+  console.log("B");
+} else if (média >= 70) {
+  console.log("C");
+} else if (média >= 60) {
+  console.log("D");
+} else if (média >= 50) {
+  console.log("E");
+} else {
+  console.log("F");
+};
+
+//
+
+const a = 8;
+const b = 3;
+const c = 2;
+
+let isEven = false;
+
+if ((a % 2 === 0 || b % 2 === 0 || c % 2 === 0)) {
+  isEven = true;
+};
+console.log(isEven);
+
+//
+
+const a = 8;
+const b = 3;
+const c = 2;
+
+let isOdd = false;
+
+if ((a % 2 !== 0 || b % 2 !== 0 || c % 2 !== 0)) {
+  isOdd = true;
+};
+console.log(isOdd);
+
+//
+
+const costOfProduct = 3;
+const saleValue = 9;
+
+if (costOfProduct >= 0 && saleValue >= 0) {
+  const totalCostOfProduct = costOfProduct * 1.2;
+  const totalProfit = (saleValue - totalCostOfProduct) * 1000;
+  console.log(totalProfit);
+} else {
+  console.log("Error, os valores não podem ser negativos");
+};
