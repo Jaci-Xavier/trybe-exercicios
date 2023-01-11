9b782525d719b7e5ff04b2fc921edf32904c9ca7
Jaci Xavier <ourivesjr@gmail.com>
2023-01-11 03:09:03 -0300

    resolvendo ultimo bonus

--- fundamentos/secao-05-html-e-css:Forms-flexbox-e-responsivo/dia-01-html-e-css-forms/Exercicios/script.js
+++ fundamentos/secao-05-html-e-css:Forms-flexbox-e-responsivo/dia-01-html-e-css-forms/Exercicios/script.js
@@ -1,1 +1,8 @@
-function
+function manualSubmit(event) {
+  event.preventDefault();
+}
+
+window.onload = function() {
+  const submitBtn = document.getElementById('submit');
+  submitBtn.addEventListener('click', manualSubmit());
+}
